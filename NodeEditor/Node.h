24853aa06d77fd2151fcae66402fb18b816758ba
Engin Manap <enginmanap@gmail.com>
2019-04-02 02:12:51 +0300

    Add Node types that would be more useful for testing

--- NodeEditor/Node.h
+++ NodeEditor/Node.h
@@ -17,6 +17,7 @@ const ImVec2 NODE_WINDOW_PADDING(8.0f, 8.0f);
 
 struct NodeType {
     const char *name;
+    bool editable;
     ConnectionDesc inputConnections[MAX_CONNECTION_COUNT];
     ConnectionDesc outputConnections[MAX_CONNECTION_COUNT];
 };
@@ -27,6 +28,19 @@ static struct NodeType s_nodeTypes[] =
                 // Math
                 {
                         "Texture",
+                        false,
+                        // Input connections
+                        {
+                                {"Write", ConnectionType_Vec3},
+                        },
+                        // Output
+                        {
+                                {"Read", ConnectionType_Vec3},
+                        },
+                },
+                {
+                        "PointShadows",
+                        false,
                         // Input connections
                         {
                         },
@@ -35,8 +49,71 @@ static struct NodeType s_nodeTypes[] =
                                 {"Out", ConnectionType_Vec3},
                         },
                 },
+
+                {
+                        "WorldRender",
+                        false,
+                        // Input connections
+                        {
+                                {"Point Shadows", ConnectionType_Vec3},
+                                {"Directional Shadows", ConnectionType_Vec3},
+                        },
+                        // Output
+                        {
+                                {"Diffuse and Specular", ConnectionType_Vec3},
+                                {"Ambient", ConnectionType_Vec3},
+                                {"Normal", ConnectionType_Vec3},
+                                {"Depth", ConnectionType_Vec3},
+                        },
+                },
+                {
+                        "SSAO Generation",
+                        false,
+                        // Input connections
+                        {
+                                {"Noise", ConnectionType_Vec3},
+                                {"Normal", ConnectionType_Vec3},
+                                {"Depth", ConnectionType_Vec3},
+                        },
+                        // Output
+                        {
+                                {"SSAO", ConnectionType_Vec3},
+                        },
+                },
+
+                {
+                        "SSAO Blur",
+                        false,
+                        // Input connections
+                        {
+                                {"SSAO", ConnectionType_Vec3},
+                        },
+                        // Output
+                        {
+                                {"Blurred SSAO", ConnectionType_Vec3},
+                        },
+                },
+
+                {
+                        "Combine",
+                        false,
+                        // Input connections
+                        {
+                                {"Diffuse and Specular", ConnectionType_Vec3},
+                                {"Ambient", ConnectionType_Vec3},
+                                {"SSAO Blurred", ConnectionType_Vec3},
+                                {"Depth", ConnectionType_Vec3},
+                        },
+                        // Output
+                        {
+                        },
+                },
+
+
+
                 {
                         "Multiply",
+                        true,
                         // Input connections
                         {
                                 {"Input1", ConnectionType_Float},
@@ -50,6 +127,7 @@ static struct NodeType s_nodeTypes[] =
 
                 {
                         "Add",
+                        true,
                         // Input connections
                         {
                                 {"Input1", ConnectionType_Float},
@@ -63,6 +141,7 @@ static struct NodeType s_nodeTypes[] =
 
                 {
                         "Divide",
+                        true,
                         // Input connections
                         {
                                 {"Input1", ConnectionType_Float},
@@ -85,16 +164,23 @@ class Node {
     std::string name;
     std::vector<Connection *> inputConnections;
     std::vector<Connection *> outputConnections;
+    bool editable;
 
     void setupConnections(std::vector<Connection *> &connections, const ConnectionDesc *connectionDescs, Connection::Types connectionType);
+
+    void initialize(uint32_t id, const ImVec2 &pos, const NodeType *nodeType);
+
+    /**
+     * This method calculates node size and connection positions,
+     * Node name or connection changes should trigger this method
+     */
+    void calculateAndSetDrawInformation();
 public:
     Node(uint32_t id, ImVec2 pos, NodeType *nodeType);
 
     Node(uint32_t id, ImVec2 pos, const char *name, uint32_t &error);
 
     bool isHovered(ImVec2 offset);//offset is scrool information
-
-    void initialize(uint32_t id, const ImVec2 &pos, const NodeType *nodeType);
 
     void display(ImDrawList *drawList, ImVec2 offset, bool dragNodeConnected);
 
@@ -116,12 +202,9 @@ public:
 
     void addOutput(const ConnectionDesc &description);
 
-
-    /**
-     * This method calculates node size and connection positions,
-     * Node name or connection changes should trigger this method
-     */
-    void calculateAndSetDrawInformation();
+    bool getEditable() {
+        return this->editable;
+    }
 };
 
 
